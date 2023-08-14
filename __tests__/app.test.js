3df917adfc1e6356048bd002181562663e48c7b1
kieran <kieran-mcdonagh@outlook.com>
2023-08-14 15:36:10 +0100

    adds GET /api/articles/:article_id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -47,3 +47,38 @@ describe("GET /api", () => {
       });
   });
 });
+
+describe("GET /api/articles/:article_id", () => {
+  test("200: should respond with an article object with all relevant properties", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then(({ body }) => {
+        const { article } = body;
+        expect(article).toHaveProperty("author");
+        expect(article).toHaveProperty("title");
+        expect(article).toHaveProperty("article_id");
+        expect(article).toHaveProperty("body");
+        expect(article).toHaveProperty("topic");
+        expect(article).toHaveProperty("created_at");
+        expect(article).toHaveProperty("votes");
+        expect(article).toHaveProperty("article_img_url");
+      });
+  });
+  test("400: should return bad request if given an invalid article id", () => {
+    return request(app)
+      .get("/api/articles/hello")
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Bad request");
+      });
+  });
+  test("404: should return 404 not found if given valid id but no data", () => {
+    return request(app)
+      .get("/api/articles/9999")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Not found");
+      });
+  });
+});
--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const { getTopics } = require("./controllers/topics.controllers");
 const { getEndpoints } = require("./controllers/endpoints.controller");
+const { getAtricleById } = require("./controllers/articles.controller");
+const { handle400s, handleCustomErrors } = require("./controllers/error.controller");
 const app = express();
 
 app.use(express.json());
 
-app.get('/api', getEndpoints)
+app.get("/api", getEndpoints);
 
 app.get("/api/topics", getTopics);
 
+app.get("/api/articles/:article_id", getAtricleById);
+
+app.use(handle400s);
+
+app.use(handleCustomErrors)
 
 app.use((err, req, res, next) => {
   res.status(500).send({ msg: err });
