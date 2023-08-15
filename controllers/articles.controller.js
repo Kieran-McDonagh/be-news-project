847c78f0ed38a3632050a71e79f24b96354ef132
kieran <kieran-mcdonagh@outlook.com>
2023-08-15 13:38:02 +0100

    adds GET /api/articles

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -1,10 +1,16 @@
-const { fetchArticleById } = require("../models/articles.model");
+const { selectArticleById, selectAllArticles } = require("../models/articles.model");
 
 exports.getArticleById = (req, res, next) => {
   const { article_id } = req.params;
-  fetchArticleById(article_id)
+  selectArticleById(article_id)
     .then((article) => {
       res.status(200).send({ article });
     })
     .catch(next);
 };
+
+exports.getAllArticles = (req, res, next) => {
+  selectAllArticles().then((articles) => {
+    res.status(200).send({articles})
+  })
+}
